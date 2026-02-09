befb154549475a237bdd0ced0b6fa4f0e2ef07e9
eldkfnrk <johongjin01@gmail.com>
2026-02-09 10:48:05 +0900

    Make moving map system and apply cinemachine.
    Make moving map system because i want to make unlimit map.
    Install and Apply cinemachine package.

---
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Player player;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+}
